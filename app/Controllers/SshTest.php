11752626cd601f837e9f3190f402f555a50a05d1
M K <mammadkouchenani@gmail.com>
2025-12-28 00:00:02 +0330

    test ssh with php

--- app/Controllers/SshTest.php
+++ app/Controllers/SshTest.php
@@ -8,28 +8,48 @@ class SshTest extends Controller
 {
     public function index()
     {
+//        $host = '5.161.144.182';
+//        $user = 'root';
+//        $key = '/var/www/.ssh/cross-servers.key';
+//        $command = '/root/scripts/test.sh';
+//
+//        $conn = ssh2_connect($host, 22);
+//        if (!$conn) {
+//            return "SSH connection failed";
+//        }
+//
+//        ssh2_auth_pubkey_file(
+//            $conn,
+//            $user,
+//            $key . '.pub',
+//            $key
+//        );
+//
+//        $stream = ssh2_exec($conn, $command);
+//        stream_set_blocking($stream, true);
+//        $output = stream_get_contents($stream);
+//        fclose($stream);
+//
+//        return nl2br($output);
+
         $host = '5.161.144.182';
         $user = 'root';
-        $key = '/var/www/.ssh/private_hetzner2.key';
-        $command = '/root/scripts/test.sh';
+        $priv = '/var/www/.ssh/cross-servers.key';
+        $pub  = '/var/www/.ssh/cross-servers.key.pub';
 
         $conn = ssh2_connect($host, 22);
         if (!$conn) {
-            return "SSH connection failed";
+            die('connect failed');
         }
 
-        ssh2_auth_pubkey_file(
-            $conn,
-            $user,
-            $key . '.pub',
-            $key
-        );
+        if (!ssh2_auth_pubkey_file($conn, $user, $pub, $priv)) {
+            die('auth failed');
+        }
 
-        $stream = ssh2_exec($conn, $command);
+        $stream = ssh2_exec($conn, 'bash /root/scripts/test.sh');
         stream_set_blocking($stream, true);
-        $output = stream_get_contents($stream);
-        fclose($stream);
+        echo stream_get_contents($stream);
 
-        return nl2br($output);
+
     }
 }
