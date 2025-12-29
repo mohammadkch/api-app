ad925cce1eed11e7e9f8f11ace9ba5967d7428c9
M K <mammadkouchenani@gmail.com>
2025-12-29 13:34:27 +0330

    delete openvpn test

--- app/Controllers/SshTest.php
+++ app/Controllers/SshTest.php
@@ -78,7 +78,7 @@ class SshTest extends Controller
 
         $client = 'ali';
         $command = "bash /root/scripts/add-openvpn.sh $client";
-        $delete_command = "sudo /opt/scripts/revoke_client.sh " . escapeshellarg($client);
+        $delete_command = "bash /root/scripts/add-openvpn.sh " . escapeshellarg($client);
 
         $stream = ssh2_exec($conn, $delete_command);
 
